faef8641153e3c310466a9be9776c6097b417bff
Kevin Lopes <kevinlopes2304@gmail.com>
2025-07-26 16:21:14 -0300

    Configurando a estrutura inicial do projeto

---
+++ CadastroClientesWinForms/CadastroClientesWinForms/Data/Database.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace CadastroClientesWinForms
+{
+    public static class Database
+    {
+        private static string connString = "Host=localhost;Port=5432;Username=postgres;Password=sua_senha;Database=cadastro_clientes";
+
+        public static NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(connString);
+        }
+    }
+}
